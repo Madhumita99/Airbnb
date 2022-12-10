64f89194299b5f6d4229f3db3e3c0438c285a7cd
Madhumita99 <42217182+Madhumita99@users.noreply.github.com>
2022-12-10 11:12:16 -0600

    Create auth.js

---
+++ DragonFly/Server/routes/middleware/auth.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+
+
+const verifyToken = (req, res, next) => {
+
+
+  const token = req.body.token || req.query.token || req.headers["x-access-token"] || req.header("x-auth-token");
+
+  console.log("token:" + token);
+
+if (!token){
+
+    return res.json({ error: "Token is required for authentication" } );
+}
+else{
+
+    try{
+        var decoded = jwt.verify(token, 'secretkey');
+        console.log(decoded);
+        return next();
+
+    } catch(err){
+        
+        return res.json({ error: "Invalid token" });
+    }
+  
+    
+}
+
+}
+
+module.exports = verifyToken;
