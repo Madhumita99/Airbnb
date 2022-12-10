e08ca2d999a572544a94398ae35be6dc969f77b7
Madhumita99 <42217182+Madhumita99@users.noreply.github.com>
2022-12-10 10:57:11 -0600

    Add files via upload

--- DragonFly/Client/src/App.js
+++ DragonFly/Client/src/App.js
@@ -1,7 +1,8 @@
-import './App.css';
 import React from 'react';
 import {Routes, Route} from 'react-router-dom';
+import Home from './components/Home';
 import AddProperty from './components/AddProperty';
+import UpdateProperty from './components/UpdateProperty';
 import AddReservation from './components/AddReservation'
 import SignUp from './components/Authentication/SignUp';
 import Login from './components/Authentication/Login';
@@ -15,13 +16,14 @@ import Favourites from './components/FavouriteProperty';
 function App(){
   return(
     <Routes>
-      <Route path="/" element={<Properties/>}></Route>
+      <Route path="/" element={<Home/>}></Route>
       <Route path='/register' element={<SignUp />}/>
       <Route path='/login' element={<Login />}/>
       <Route path='/hostlogin' element={<HostLogin />} />
       <Route path='/hostregister' element={<HostSignUp />} />
       <Route path='/addProperty' element={<AddProperty />} />
-      <Route path='/addReservation' element={<AddReservation />} />
+      <Route path='/updateProperty/:id' element={<UpdateProperty />} />
+      <Route path='/addReservation/:id' element={<AddReservation />} />
       <Route path="/properties" element={<Properties/>}></Route>
       <Route path="/reservations" element={<Reservations/>}></Route>
       <Route path="/welcome" element={<Welcome/>}></Route>
